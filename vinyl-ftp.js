61c98d749b5749d03cc48bad74d0363eed8e8b81
Nagy Janos <nagy@zold.lan>
2023-03-03 10:28:00 +0100

    Beállítások config fájlban

--- vinyl-ftp.js
+++ vinyl-ftp.js
@@ -1,10 +1,16 @@
 var fs = require( 'vinyl-fs' );
 var ftp = require( 'vinyl-ftp' );
+var reader = require('properties-reader');
+var config = reader('config/config.properties');
+
+const host = config.get('hosting.host');
+const user = config.get('hosting.user');
+const pass = config.get('hosting.pass');
  
 var conn = new ftp( {
-    host: 'ftpupload.net',
-    user: '',
-    password: '',
+    host: host,
+    user: user,
+    password: pass,
     parallel: 10,
     secure: true,
     secureOptions: {rejectUnauthorized: false}
